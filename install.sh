3c822922b1eb48f6eafed83418c6973cafe69766
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2020-10-01 17:48:32 +0800

    Added docker-compose install script

--- install.sh
+++ install.sh
@@ -13,3 +13,5 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
