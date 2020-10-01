b09e542598c6045187f8520ab264979eba8c4085
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2020-10-01 18:46:24 +0800

    Edited nginx script

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,12 @@
+#name of the domain by the user
+echo Please enter the domain name, without 'www'
+read domain
+echo The Doamin is $domain
+echo Please enter the port where the website will be served:
+read port
+echo The website will be served at $domain:$port
+
+# Next lets create the appropraite file
 sudo touch /etc/nginx/sites-available/$domain
 #Let's edit the nginx file
 content="server {
