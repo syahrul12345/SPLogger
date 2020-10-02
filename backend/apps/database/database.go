b628e5abb959b9ef1243e64c93c1597c5d56ba26
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2020-10-02 13:05:11 +0800

    Edited database to accept connections from dev server

--- backend/apps/database/database.go
+++ backend/apps/database/database.go
@@ -1,10 +1,10 @@
 package database
 
 import (
-	"spdigital/apps/database/models"
 	"fmt"
 	"log"
 	"os"
+	"spdigital/apps/database/models"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -15,11 +15,19 @@ var db *gorm.DB //database
 
 func init() {
 	godotenv.Load()
-	dbHost := os.Getenv("DB_HOST")
+	var dbHost string
+	var port string
+	if os.Getenv("PROD") == "true" {
+		dbHost = os.Getenv("DB_HOST")
+		port = os.Getenv("DB_PORT")
+	} else {
+		dbHost = "localhost"
+		port = "5433"
+	}
 	username := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASS")
 	dbName := os.Getenv("DB_NAME")
-	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", dbHost, username, dbName, password, port) //Build connection string
 	log.Printf("Attempting to connect to %s\n", dbURI)
 	conn, err := gorm.Open("postgres", dbURI)
 	if err != nil {
